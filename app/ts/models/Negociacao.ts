98462c4dc7be9aab702c2b739fe8de94fa85d8f9
Breno Martins <brenomsimao123@outlook.com>
2021-05-14 23:05:22 -0300

    Added interfaces Using extends on Interfaces to easily imports a group of them

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,6 +1,6 @@
+import { iMeuObjeto } from './MeuObjeto'
 
-
-export class Negociacao {
+export class Negociacao implements iMeuObjeto<Negociacao> {
 
     constructor(readonly data:Date, readonly quantidade:number, readonly valor:number){
         
@@ -8,6 +8,21 @@ export class Negociacao {
 
     get volume (){
         return this.quantidade * this.valor
+    }
+
+    paraTexto():void{
+        console.log(
+            `Data: ${this.data}
+            Quantidade: ${this.quantidade}, 
+            Valor: ${this.valor}, 
+            Volume: ${this.volume}`
+        )
+    }
+
+    equals(negociacao:Negociacao):boolean{
+        return this.data.getDate() == negociacao.data.getDate()
+            && this.data.getMonth() == negociacao.data.getMonth()
+            && this.data.getFullYear() == negociacao.data.getFullYear();
     }
 
 }
